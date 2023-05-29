35cb39236ca95efab53acc3593b1efb68d1191c0
Aditya Agarwal <adta@gamil.com>
2023-05-29 10:16:53 +0530

    first commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+
+    return (
+        <div className="footer-main">
+            <div className="footer-text">
+                <p className="footer-p">
+                Made with <span class="love">❤</span> by
+                <a className="footer-a" href="https://www.linkedin.com/in/aditya-agarwal-8852b3253/"> Aditya Agarwal</a>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://www.allrecipes.com/thmb/dgnxqxdc3s1YI1CE-1_O-hu9dnU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ar_placeholder-6681e9afa32045a78d8f2632de8b28b7.jpg';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
