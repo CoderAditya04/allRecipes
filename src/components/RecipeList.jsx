386bd27fef794cf7c84e967324f2b9cbc4078311
aditya agarwal <t-Aditya4.agarwal@ocltp.com>
2024-03-23 16:35:57 +0530

    added pagination

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,26 +1,63 @@
 import { Container, Header, Grid } from "semantic-ui-react";
 import RecipeListItem from "./RecipeListItem";
+import { useState } from "react";
+import "./style.css";
 
 const RecipeList = ({ recipes, searchedQuery }) => {
+  const [page, setPage] = useState(1);
+  const len = recipes.length;
+  const len2 = Math.ceil(len / 5);
+  const selectPageHandler = (selectedPage) => {
+    if (selectedPage >= 1 && selectedPage <= len2 && selectedPage !== page) {
+      setPage(selectedPage);
+    }
+  };
+  return (
+    <Container>
+      <Header
+        size="huge"
+        content={`RECIPE LIST FOR ${searchedQuery}`}
+        textAlign="center"
+      />
+      <Grid columns={3} doubling>
+        {recipes &&
+          recipes.slice(page * 5 - 5, page * 5).map((recipe, index) => (
+            <Grid.Column key={index}>
+              <RecipeListItem recipe={recipe} />
+            </Grid.Column>
+          ))}
+      </Grid>
+      {len > 0 && (
+        <div className="pagination">
+          <span
+            className={page > 1 ? "" : "pagination__disable"}
+            onClick={() => selectPageHandler(page - 1)}
+          >
+            ◀
+          </span>
 
-    return (
-        <Container>
-            <Header 
-                size="huge"
-                content={`RECIPE LIST FOR ${searchedQuery}`}
-                textAlign='center'
-            />
-            <Grid columns={4} doubling>
-                { 
-                    recipes && recipes.map(recipe => (
-                        <Grid.Column>
-                            <RecipeListItem recipe={recipe} />
-                        </Grid.Column>
-                    ))
-                }
-            </Grid>
-        </Container>
-    )
-}
+          {[...Array(len2)].map((_, i) => {
+            return (
+              <span
+                key={i}
+                className={page === i + 1 ? "pagination__selected" : ""}
+                onClick={() => selectPageHandler(i + 1)}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+
+          <span
+            className={page < len2 ? "" : "pagination__disable"}
+            onClick={() => selectPageHandler(page + 1)}
+          >
+            ▶
+          </span>
+        </div>
+      )}
+    </Container>
+  );
+};
 
 export default RecipeList;
